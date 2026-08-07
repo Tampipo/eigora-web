e3178480244fe0a84e11b4a1ca4782f1398488c1
Tanguy <tanguy.marsault02@gmail.com>
2026-08-07 23:10:02 +0200

    Align the spin page header with the rename

--- app/[locale]/qm/spin/page.tsx
+++ app/[locale]/qm/spin/page.tsx
@@ -1,4 +1,4 @@
-// Copyright (C) 2026 Tanguy Marsault - PhySense
+// Copyright (C) 2026 Tanguy Marsault - Eigora
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { setRequestLocale } from "next-intl/server";
